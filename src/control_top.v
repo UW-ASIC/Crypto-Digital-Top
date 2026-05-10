fe022949809dd0f6ad48d61fa2227754d4086933
Ada Mahdavi <ayuyu.wrd@gmail.com>
2026-05-09 18:55:43 -1000

    re-gating dequeue

--- src/control_top.v
+++ src/control_top.v
@@ -83,8 +83,12 @@ wire [1:0] counter_top_unused;
   .valid_in_aes(compq_aes_valid), .valid_in_sha(compq_sha_valid), .dest_addr_aes(compq_aes_data), 
   .dest_addr_sha(compq_sha_data), .ready_out_aes(compq_ready_aes), .ready_out_sha(compq_ready_sha), 
   .data_out(compq_data), .valid_out(compq_valid_out), .ready_in(compq_ready_in));
-
-serializer #(.ADDRW(ADDRW)) serializer_inst(.clk(clk), .rst_n(rst_n), .n_cs(cs_n), .spi_clk(spi_clk), 
-  .valid_in(compq_valid_out), .addr(compq_data), .miso(miso), .ready_out(compq_ready_in));
+  
+  wire ser_ready_out;
+  assign compq_ready_in = ser_ready_out & ~cs_n;
+  serializer #(.ADDRW(ADDRW)) serializer_inst(.clk(clk), .rst_n(rst_n), .n_cs(cs_n), .spi_clk(spi_clk), 
+  .valid_in(compq_valid_out), .addr(compq_data), .miso(miso), .ready_out(ser_ready_out));
+  
   wire _unused = &{counter_top_unused,curr_mode_top_unused};
+  
 endmodule
